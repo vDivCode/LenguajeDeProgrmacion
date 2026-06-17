292dc2f7a70bbeb9f92691ad5e0f29ad05cb517a
davidventuraguzman <david.02.vg@gmail.com>
2026-06-17 10:22:41 -0500

    Remove old v1 folders, keeping Stefano's v2 structure

--- career_recommender/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from recommender import vistas
-
-urlpatterns = [
-    path("", vistas.inicio, name="index"),
-    path("recommend/", vistas.recomendar, name="recommend"),
-]
